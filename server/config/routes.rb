f79c509ad4ba790529fe9a4245b32160cabb75e1
Devin <devinmui@yahoo.com>
2016-05-21 14:26:54 -0700

    foobar

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :families
       resources :showers do
         member do
           get 'time_stamp_start'
